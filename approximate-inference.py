dad2be9d991157b58fe493c603102b49870b2aa1
Quynh (Christina) Vu <pquynhvu@outlook.com>
2024-01-12 13:24:54 -0500

    approximate-inference.py

--- approximate-inference.py
+++ approximate-inference.py
@@ -12,3 +12,11 @@ import random
 from torch.distributions.normal import Normal
 from functools import partial
 import matplotlib.pyplot as plt
+
+wget.download("https://erdogdu.github.io/sta414/hw/hw2/chess_games.csv")
+games = pd.read_csv("chess_games.csv")[["winner_index", "loser_index"]].to_numpy()
+wget.download("https://erdogdu.github.io/sta414/hw/hw2/chess_players.csv")
+names = pd.read_csv("chess_players.csv")[["index", "player_name"]].to_numpy()
+
+games = torch.IntTensor(games)
+
