27d7d40d159b3c079b8d39389ea69e307472c842
Quynh (Christina) Vu <pquynhvu@outlook.com>
2024-01-12 18:58:38 -0500

    em-algorithm.py

--- em-algorithm.py
+++ em-algorithm.py
@@ -2,7 +2,7 @@ import scipy
 import numpy as np
 import itertools
 import matplotlib.pyplot as plt
-# from numpy.core import matrix_multiply as mm
+from numpy import matmul as mm
 
 # Generating the Data
 # Set the number of points N=400, their dimension  D=2, and the number of clusters  K=2 , Sample 200 data points for k=1 and 200 for k=2
@@ -31,92 +31,109 @@ plt.plot(np.array([0.1, 6.0]), np.array([0.1, 0.1]), '^', color = "crimson", lab
 plt.legend()
 plt.show()
 
-## Implement and Run K-Means algorithm
+#  Implement EM algorithm for Gaussian mixtures
 
-def cost(data, R, Mu):
-    D = data.ndim
-    N = data.shape[0]
-    K = data.shape[1]
-    J = 0
-    for i in range(N):
-      J += sum(((data[i] - Mu.T)**2).sum(axis=1)*R[i])
-    return J
+def normal_density(x, mu, Sigma):
+    return np.exp(-.5 * np.dot(x - mu, np.linalg.solve(Sigma, x - mu))) \
+        / np.sqrt(np.linalg.det(2 * np.pi * Sigma))
 
-## K-Means assignment 
-
-def km_assignment_step(data, Mu):
-    D = data.ndim       # dimension of datapoint
+def log_likelihood(data, Mu, Sigma, Pi):
     N = data.shape[0]   # Number of datapoints
-    K = data.shape[1]   # number of clusters
-    r = []
-    for i in range(N):
-        r.append(np.sqrt(((data[i] - Mu.T)**2).sum(axis=1))) # distances fron each datapoints to each cluster means
+    D = data.ndim       # dimension of datapoint
+    K = Mu.shape[1]     # number of mixtures
+    L, T = 0., 0. 
+    for n in range(N):
+      sum = 0
+      for k in range(K):
+        sum += Pi[k]*normal_density(data[n,:], Mu[:,k], Sigma[k])
+      L += np.log(sum)
+    return L
+
+def gm_e_step(data, Mu, Sigma, Pi):
+    N = data.shape[0]   # Number of datapoints
+    D = data.ndim       # dimension of datapoint
+    K = Mu.shape[1]     # number of mixtures
+    Gamma = np.zeros((N, K))
+    for n in range(N):
+      for k in range(K):
+        Gamma[n, k] = Pi[k]*normal_density(data[n,:], Mu[:,k], Sigma[k])
+    Gamma = Gamma/Gamma.sum(axis=1)[:, None]
+    return Gamma
+
+def gm_m_step(data, Gamma):
+    N = data.shape[0]   # Number of datapoints
+    D = data.ndim       # dimension of datapoint
+    K = Gamma.shape[1]  # number of mixtures
+    Nk = Gamma.sum(axis = 0)
+    Mu = np.dot(data.T, Gamma)/Nk
+    Pi = Nk/N
     
-    r = np.asarray(r) 
-    arg_min = np.argmin(r, axis=1) # index where r has the lower value for each obs
-    R_new = np.zeros_like(r) # initialize matrix R_new 
-    R_new[np.arange(len(R_new)), arg_min] = 1
-    return R_new
+    Sigma = np.zeros((K, D, D))
+    for k in range(K):
+      y = data - Mu[:,k]
+      Sigma[k] = (Gamma[:,k,None,None]*mm(y[:,:,None], y[:,None,:])).sum(axis = 0)
 
-## K-Means refitting
+    Sigma /= Gamma.sum(axis=0)[:,None,None]
+    return Mu, Sigma, Pi
 
-def km_refitting_step(data, R, Mu):
-    D = data.ndim       # dimension of datapoint
-    N = data.shape[0]   # Number of datapoints
-    K = data.shape[1]   # number of clusters
-    Mu_new = []
-    for i in range(D):
-      Mu_new.append(np.dot(R[:,i], data)/sum(R[:,i]))
-    Mu_new = np.asarray(Mu_new).T.reshape(D, K)
-    return Mu_new
+def gm_m_step(data, Gamma):
+    N, D = data.shape 
+    K = Gamma.shape[1]  
+    Nk = np.sum(Gamma, axis = 0) 
+    Mu = np.asarray([np.sum(data * (Gamma[:, k] / Nk[k])[:, np.newaxis], axis = 0) for k in range(K)]).T
+    Sigma = [0]*K # TODO
+    for k in range(K):
+        Sigma[k] = np.sum((Gamma[:, k]/Nk[k])[:, np.newaxis, np.newaxis] * ((data - Mu[:, k])[:,:, None]*(data - Mu[:, k])[:,None, :]), axis = 0)
+    
+    Pi = Nk/N # TODO
+    return Mu, Sigma, Pi
 
-## Call the K-Means algorithm.
-
-N, D = 400, 2
-K = 2
-max_iter = 100
-class_init = np.random.randint(2, size=N) 
-R = np.zeros((N, K))
-R[class_init == 1, 1] = 1
-R[:,0] = 1 - R[:, 1]
-
-class_1_true_ix = np.asarray(np.where(labels == 0))
-class_2_true_ix = np.asarray(np.where(labels == 1))
-
+N = data.shape[0]   # number of datapoints
+D = data.ndim       # dimension of datapoint
+K = data.shape[1]   # number of mixtures
 Mu = np.zeros([D, K])
 Mu[:, 1] = 1.
-R.T.dot(data), np.sum(R, axis=0)
-c = []
+Sigma = [np.eye(2), np.eye(2)]
+Pi = np.ones(K) / K
+Gamma = np.zeros([N, K]) # Gamma is the matrix of responsibilities 
+max_iter  = 200
+mus  = []
+Sigmas = []
+Pis = []
 for it in range(max_iter):
-    R = km_assignment_step(data, Mu)
-    Mu = km_refitting_step(data, R, Mu)
-    c.append(cost(data, R, Mu))
-    print(it, cost(data, R, Mu))
-class_1_KM = data[np.where(R[:,0] == 1)]
-class_2_KM = data[np.where(R[:,1] == 1)]
+    Gamma = gm_e_step(data, Mu, Sigma, Pi)
+    Mu, Sigma, Pi = gm_m_step(data, Gamma)
+    mus.append(Mu)
+    Sigmas.append(Sigma)
+    Pis.append(Pi)
 
-## Scatterplot for the data points showing the K-Means cluster assignments of each point.
+arg_max = np.argmax(Gamma, axis=1)
+class_1_EM = data[np.where(arg_max == 0)]
+class_2_EM = data[np.where(arg_max == 1)]
 
-ix_KM = list(range(100))
-fig, (ax1, ax2) = plt.subplots(1,2)
+print("Misclassification error of EM is ", np.mean(arg_max!=labels))
 
-ax1.plot(class_1_KM[:,0], class_1_KM[:,1], 'x', color='darkorange', label ='First class') 
-ax1.plot(class_2_KM[:,0], class_2_KM[:,1], 'o', color='darkorchid', label ='Second class')
+log_lik = []
+for i in range(200):
+  log_lik.append(log_likelihood(data, mus[i], Sigmas[i], Pis[i]))
+ix_EM = list(range(200))
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+
+ax1.plot(class_1_EM[:,0], class_1_EM[:,1], 'x', color='darkorange', label ='First class') 
+ax1.plot(class_2_EM[:,0], class_2_EM[:,1], 'o', color='darkorchid', label ='Second class')
 ax1.plot(np.array([0.1, 6.0]), np.array([0.1, 0.1]), '^', color = "crimson", label = "True means")
 ax1.plot(Mu[0,:], Mu[1,:], 'P', color = "g", label = "Estimated means")
-ax1.set_title('K-means cluster assignment')
+ax1.set_title('EM-algorithm cluster assignment')
 
-ax2.plot(ix_KM, c, color='darkorange') 
-ax2.set_title('Cost of K-means over 100 iterations')
+ax2.plot(ix_EM, log_lik, color='darkorange') 
+ax2.set_title('Log-likelihood of EM over 200 iterations')
 
 fig.tight_layout()
 plt.show()
 
-new_label = []
-for i in range(N):
-  if R[i, 0] == 1:
-    new_label.append(0)
-  elif R[i, 1] == 1:
-    new_label.append(1)
-new_label = np.asarray(new_label)
-print("Misclassification error of K-means is ", np.mean(new_label != labels))
+# K-means might do better if we add more clusters, as the more clusters we add, the easier it is for the algorithm to reduce 
+# the distance between points and centroids, reducing the within variability while the variability between clusters is likely 
+# to increase. In EM-algorithm, as we increase the number of clusters, the number of local maxima grows quickly and thus a local 
+# maximizer obtained by the EM algorithm can be a very bad estimator that is far away from the MLE.
+
